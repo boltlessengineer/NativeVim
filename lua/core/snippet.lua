7bf35d12170f621c64017bb0e892e1a6eacf9607
Seongmin Lee <boltlessengineer@gmail.com>
2024-07-15 11:16:25 +0000

    feat: add snippet examples

--- lua/core/snippet.lua
+++ lua/core/snippet.lua
@@ -1,4 +1,11 @@
-function vim.snippet.add(trigger, snippet, opts)
+---@param trigger string trigger string for snippet
+---@param body string snippet text that will be expanded
+---@param opts? vim.keymap.set.Opts default to `{ buffer = 0 }`
+---
+---Refer to <https://microsoft.github.io/language-server-protocol/specification/#snippet_syntax>
+---for the specification of valid body.
+function vim.snippet.add(trigger, body, opts)
+    opts = opts or { buffer = 0 }
     vim.keymap.set("ia", trigger, function()
         -- If abbrev is expanded with keys like "(", ")", "<cr>", "<space>",
         -- don't expand the snippet. Only accept "<c-]>" as trigger key.
@@ -7,6 +14,8 @@ function vim.snippet.add(trigger, snippet, opts)
             vim.api.nvim_feedkeys(trigger .. c, "i", true)
             return
         end
-        vim.snippet.expand(snippet)
+        vim.snippet.expand(body)
     end, opts)
 end
+
+-- see after/ftplugin/lua.lua for examples 
