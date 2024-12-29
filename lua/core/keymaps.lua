65b4c5f35a6f8399e5a5f99fbaedd6e6b8952989
Seongmin Lee <boltlessengineer@gmail.com>
2024-12-29 19:37:08 +0000

    feat(keymaps): add missing snippet keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -6,3 +6,18 @@ vim.keymap.set({ "n", "x" }, "]e", function() vim.diagnostic.goto_next({ severit
 vim.keymap.set({ "n", "x" }, "[e", function() vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR }) end, { desc = "Prev Error" })
 vim.keymap.set({ "n", "x" }, "]w", function() vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.WARN }) end, { desc = "Next Warning" })
 vim.keymap.set({ "n", "x" }, "[w", function() vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.WARN }) end, { desc = "Prev Warning" })
+
+vim.keymap.set({ "i", "s" }, "<tab>", function()
+    if vim.snippet.active({ direction = 1 }) then
+        return "<cmd>lua vim.snippet.jump(1)<cr>"
+    else
+        return "<tab>"
+    end
+end, { desc = "snippet jump", expr = true })
+vim.keymap.set({ "i", "s" }, "<s-tab>", function()
+    if vim.snippet.active({ direction = -1 }) then
+        return "<cmd>lua vim.snippet.jump(-1)<cr>"
+    else
+        return "<tab>"
+    end
+end, { desc = "snippet jump backward", expr = true })
